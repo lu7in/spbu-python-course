b2b419386344f12da729e052199c49884e59b175
Dan <43rv.jw@gmail.com>
2025-10-02 16:52:51 +0300

    Add descriptions for modules and functions, add more tests, change angle function in vectors.py

--- tests/test_vectors.py
+++ tests/test_vectors.py
@@ -1,3 +1,5 @@
+import pytest
+
 from project import vectors
 from typing import Union
 
@@ -17,8 +19,29 @@ def test_length() -> None:
     assert result == 5  # sqrt(3^2 + 4^2) = 5
 
 
-def test_angle() -> None:
+def test_non_parallel_vectors_angle() -> None:
     v1: list[Number] = [1, 0]
     v2: list[Number] = [0, 1]
     result = vectors.angle(v1, v2)
     assert result == 90.0  # acos(0/2) ~= 1,5 радиан = 90 градусов
+
+
+def test_collinear_vectors_angle() -> None:
+    v1: list[Number] = [1, 1]
+    v2: list[Number] = [2, 2]
+    result = vectors.angle(v1, v2)
+    assert result == 0.0
+
+
+def test_vectors_different_dimensions_error() -> None:
+    v1: list[Number] = [1, 2, 3]
+    v2: list[Number] = [4, 5]
+    with pytest.raises(ValueError):
+        vectors.angle(v1, v2)
+
+
+def test_vectors_zero_length_error() -> None:
+    v1: list[Number] = [1, 2, 3]
+    v2: list[Number] = [0, 0]
+    with pytest.raises(ValueError):
+        vectors.angle(v1, v2)
